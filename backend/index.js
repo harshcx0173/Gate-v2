00a6b66cfbe79b791c12253b91cb3b13ced39373
harsh <harshcx0173@gmail.com>
2025-07-09 15:33:17 +0530

    3.6

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("MongoDB connected");
 
   server.listen(8000, '0.0.0.0', () => {
-    console.log("Server running on http://localhost:8000");
+    console.log(`Server is running on port ${PORT}`)
   });
 });
 
